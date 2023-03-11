594949f2875bda2dc465f8d5179be6c774738011
chrzanowskikamil <kamil-chrzanowski@gazeta.pl>
2023-03-11 20:34:33 +0100

    Refactor Menu.tsx componenet

--- src/components/Navigation/Menu.tsx
+++ src/components/Navigation/Menu.tsx
@@ -2,6 +2,32 @@ import { Flex, Link } from '@chakra-ui/react';
 import { Link as ReachLink } from 'react-router-dom';
 
 export const Menu = () => {
+  const ctas = [
+    {
+      tabIndex: 2,
+      title: 'Home',
+      to: '/',
+    },
+    {
+      tabIndex: 3,
+      title: 'About',
+      to: '/about',
+    },
+    {
+      tabIndex: 4,
+      title: 'Contact',
+      to: '/contact',
+    },
+  ];
+
+  const links = ctas.map(cta => (
+    <Link
+      as={ReachLink}
+      to={cta.to}
+      tabIndex={cta.tabIndex}>
+      {cta.title}
+    </Link>
+  ));
   return (
     <Flex
       display={{ base: 'none', md: 'flex' }}
@@ -10,24 +36,7 @@ export const Menu = () => {
       align='center'
       justify='space-evenly'
       letterSpacing={2}>
-      <Link
-        as={ReachLink}
-        to='/'
-        tabIndex={2}>
-        Home
-      </Link>
-      <Link
-        as={ReachLink}
-        to='/about'
-        tabIndex={3}>
-        About
-      </Link>
-      <Link
-        as={ReachLink}
-        to='/contact'
-        tabIndex={4}>
-        Contact
-      </Link>
+      {links}
     </Flex>
   );
 };
